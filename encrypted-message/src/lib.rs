fc9d7116f7a263dd3a182e38a267068b8b4d72ab
sams <coding_sams@tuta.io>
2024-10-10 19:36:56 +0200

    doing work

--- encrypted-message/src/lib.rs
+++ encrypted-message/src/lib.rs
@@ -1,4 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub enum Message {
     StringMessage(String),
     PublicKeyMessage([u8; 32]),
+}
+
+fn test() {
+    let t = Message::StringMessage(String::from("test"));
 }
--- encrypted-sender/src/main.rs
+++ encrypted-sender/src/main.rs
@@ -1,11 +1,47 @@
-use crypto_box::aead::OsRng;
-use crypto_box::PublicKey;
-use crypto_box::SecretKey;
+use clap::Parser;
+use crypto_box::{aead::OsRng, PublicKey, SecretKey};
+use encrypted_message::Message;
+use std::net::Ipv4Addr;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
-fn main() {
+/// A program to send an encrypted message to an receiver
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Message to send
+    #[arg(short, long)]
+    message: String,
+
+    /// IP of receiver
+    #[arg(short, long)]
+    ip: Ipv4Addr,
+
+    /// Port of receiver
+    #[arg(short, long)]
+    port: u8,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let mut stream = TcpStream::connect(format!("{}:{}", args.ip, args.port)).await?;
+
+    let message = Message::StringMessage(args.message);
+
+    let d = serde_json::to_vec(&message)?;
+
+    stream.write_all(&d).await?;
+
+    let ip = args.ip;
+
     let alice_secret_key = SecretKey::generate(&mut OsRng);
     let alice_public_key = alice_secret_key.public_key().as_bytes().clone();
 
     let alice_public_key_receiving = PublicKey::from_bytes(alice_public_key);
     println!("Hello, world!");
+    Ok(())
 }
