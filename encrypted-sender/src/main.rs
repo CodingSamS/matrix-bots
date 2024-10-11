f83d02805cfb3f4322e4be47a4668786c982541c
SamS <coding_sams@tuta.io>
2024-10-11 15:06:21 +0200

    startet with tokio serde

--- encrypted-sender/src/main.rs
+++ encrypted-sender/src/main.rs
@@ -6,6 +6,8 @@ use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
+use tokio_serde::formats::SymmetricalBincode;
+use tokio_util::codec::{FramedWrite, LengthDelimitedCodec};
 
 /// A program to send an encrypted message to an receiver
 #[derive(Parser, Debug)]
@@ -28,18 +30,24 @@ struct Args {
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let mut stream = TcpStream::connect(format!("{}:{}", args.ip, args.port)).await?;
+    let socket = TcpStream::connect(format!("{}:{}", args.ip, args.port)).await?;
 
-    let message = Message::StringMessage(args.message);
+    // Delimit frames using a length header
+    let length_delimited = FramedWrite::new(socket, LengthDelimitedCodec::new());
 
-    let d = serde_json::to_vec(&message)?;
+    // Serialize frames with Bincode
+    let mut serialized =
+        tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalBincode::default());
 
-    stream.write_all(&d).await?;
-
-    let ip = args.ip;
-
+    // generate random key
     let alice_secret_key = SecretKey::generate(&mut OsRng);
     let alice_public_key = alice_secret_key.public_key().as_bytes().clone();
+
+    let message = Message::PublicKeyMessage(alice_public_key);
+    serialized.send();
+
+    // send public key to bob
+    stream.write_all(&serde_json::to_vec(&Message::PublicKeyMessage(alice_public_key))?)
 
     let alice_public_key_receiving = PublicKey::from_bytes(alice_public_key);
     println!("Hello, world!");
